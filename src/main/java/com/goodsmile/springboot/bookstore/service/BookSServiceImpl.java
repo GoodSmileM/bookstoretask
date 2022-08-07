b2424c16e217806a1176e1482f60096ae3dc31c8
GoodSmileM <48975986+GoodSmileM@users.noreply.github.com>
2022-08-08 01:53:46 +0800

    update service layer

--- src/main/java/com/goodsmile/springboot/bookstore/service/BookSServiceImpl.java
+++ src/main/java/com/goodsmile/springboot/bookstore/service/BookSServiceImpl.java
@@ -1,11 +1,36 @@
 package com.goodsmile.springboot.bookstore.service;
 
 import com.goodsmile.springboot.bookstore.dao.BookDao;
+import com.goodsmile.springboot.bookstore.model.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.Id;
+import java.util.Optional;
 
 @Service
 public class BookSServiceImpl implements BookService{
     @Autowired
     private BookDao bookDao;
+
+    @Override
+    public void addNewBook(Book book) {
+
+    }
+
+    @Override
+    public void addBook(Book book) {
+        bookDao.save(book);
+    }
+
+    @Override
+    public Optional<Book> findById(Long id) {
+        return bookDao.findById(id);
+
+    }
+
+    @Override
+    public Book[] getAll() {
+        return new Book[0];
+    }
 }
--- src/main/java/com/goodsmile/springboot/bookstore/service/BookService.java
+++ src/main/java/com/goodsmile/springboot/bookstore/service/BookService.java
@@ -1,4 +1,12 @@
 package com.goodsmile.springboot.bookstore.service;
 
+import com.goodsmile.springboot.bookstore.model.Book;
+
+import java.util.Optional;
+
 public interface BookService {
+    void addNewBook(Book book);
+    void addBook(Book book);
+    Optional<Book> findById(Long id);
+    Book[] getAll();
 }
